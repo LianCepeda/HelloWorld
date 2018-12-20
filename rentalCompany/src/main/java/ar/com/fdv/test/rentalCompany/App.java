d205096089a2e8a5a4d0945e12ec101674f67279
Guillermo Morzan <guillermo.morzan@gmail.com>
2018-12-20 15:27:00 -0300

    RentalCompany project structure

---
+++ rentalCompany/src/main/java/ar/com/fdv/test/rentalCompany/App.java
@@ -0,0 +1,13 @@
+package ar.com.fdv.test.rentalCompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
